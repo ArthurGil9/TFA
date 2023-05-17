84fef889eff33ff2ee9d161dda5a0367d3e376fe
ArthurGil9 <120035899+ArthurGil9@users.noreply.github.com>
2023-05-18 00:11:24 +0200

    burger menu début

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,28 @@
-console.info('Hello world');
+'use strict'
+
+const menuBurger = document.querySelector('.menu__burger');
+const menu = document.querySelector('.menu');
+
+menuBurger.addEventListener('click', function(){
+    menu.classList.toggle("menu--open");
+}
+)
+
+function solutionA(){
+    let sClass="menu--open",
+           oNavbar = document.getElementById("ancre");
+     if (oNavbar.classList.contains(sClass) == true) {
+       oNavbar.classList.remove(sClass); // Cache le menu
+     } else {
+       oNavbar.classList.add(sClass);// Affiche le menu
+     }
+ }
+ document.addEventListener('DOMContentLoaded',function(){
+   document.getElementById("ancre").addEventListener("click",solutionA); 
+ }); 
+
+
+ function myFunction() { 
+  var element = document.body; 
+  element.classList.toggle("grille");
+}
