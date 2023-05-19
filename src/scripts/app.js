fcd28cb83208714f131f336fd1b00126fe45174c
ArthurGil9 <120035899+ArthurGil9@users.noreply.github.com>
2023-05-19 12:22:41 +0200

    amélioration version mobile

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,29 @@
 'use strict'
-import Swiper from 'swiper';
-const swiper = new Swiper('.swiper-container');
-
+import { Swiper } from 'swiper';
 
 
 const menuBurger = document.querySelector('.menu__burger');
 const menu = document.querySelector('.menu');
-if(menuBurger){
+
 menuBurger.addEventListener('click', function(){
     menu.classList.toggle("menu--open");
 }
-)}
+);
+
+function solutionA(){
+    let sClass="menu--open",
+           oNavbar = document.getElementById("ancre");
+     if (oNavbar.classList.contains(sClass) == true) {
+       oNavbar.classList.remove(sClass); // Cache le menu
+     } else {
+       oNavbar.classList.add(sClass);// Affiche le menu
+     }
+ }
+ document.addEventListener('DOMContentLoaded',function(){
+   document.getElementById("ancre").addEventListener("click",solutionA); 
+ }); 
+
+ 
+const swiper = new Swiper('.swiper-container');
 
 
